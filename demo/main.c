5cfa1d854860e16c00a5a9576ff16481f5f71770
SnoWolf2018 <574366448@qq.com>
2018-11-09 21:12:11 +0800

    Initial Commit

---
+++ demo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  demo
+//
+//  Created by snowolf on 2018/11/9.
+//  Copyright © 2018 snowolf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
